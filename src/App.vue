48337211fc637592584bbb970866f67767796912
WJWYA <392154083@qq.com>
2020-12-10 18:51:10 +0800

    这是一个vue项目发布到github的测试demo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="这是一个vue项目发布到github的测试demo"/>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -3,6 +3,29 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// 导入 Element_ui
+import ElementUI from 'element-ui'
+// 导入 Element-ui 样式
+import 'element-ui/lib/theme-chalk/index.css'
+// 插件 Element-ui
+Vue.use(ElementUI)
+
+//导入路由
+import VueRouter from 'vue-router'
+//use一下
+Vue.use(VueRouter)
+// 创建路由
+const router = new VueRouter({
+  // routes: [
+  //     {//举个例子，若路由地址为····/test,则调用组件名为test的组件
+  //     path:'/test', //路由地址
+  //     component: test //组件名称
+
+  //     },
+  // ]
+})
 new Vue({
   render: h => h(App),
+   // 挂载到Vue示例上
+   router
 }).$mount('#app')
